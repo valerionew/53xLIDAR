6c82cf80c65b1b67f9094dd6c3ecc1b49e58b789
5N44P <valerionappi995@gmail.com>
2018-08-06 23:19:14 +0200

    moving firmware in it's folder

--- stm32duino-variant/README.mk
+++
@@ -1,5 +0,0 @@
-# how to use
-
-1. install stm32duino (official ST core, NOT rogerclarkmelbourne)
-1. Drop the LIDAR_F411RET folder into the `.arduino15/packages/STM32/hardware/stm32/1.3.0/variants` folder. In arudino IDE do `File` -> `Preferences` to see where the folder is
-1. Add the content of boards.txt to `.arduino15/packages/STM32/hardware/stm32/1.3.0/boards.txt`
